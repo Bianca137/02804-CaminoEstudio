1f8e7f27303f478b6eea65a07d85a911c191b33b
CicloM <CicloM@192.168.2.137>
2019-10-28 18:36:44 +0100

    :tada: Commit Inicial

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -15,15 +15,51 @@
  */
 package org.japo.java.main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author Bianca Antonela Glavan - biancaantonela.glavan.alum@iescamp.es
  */
 public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
     public static void main(String[] args) {
-        
-    
-        
+
+        // Promedio diario en Kilómetros que recorre al isntituto, ida y vuelta.
+        //Distancia entre Sagunto y Santiago son 980Km.
+        // Calcular y mostrar el numero de días de clase, en los que la distancia acumulada en sus desplazamientos bastaría para recorrer el camino Santiago.
+        final double ida;
+        final double vuelta;
+        final double distanciasS = 980;
+
+        //jornadas = CAMINO / traslado;
+        try {
+            System.out.print("Introduce los kilómetros de ida al instituto: ");
+            ida = SCN.nextInt();
+
+            System.out.print("Introduce los kilómetros de vuleta al instituto: ");
+            vuelta = SCN.nextInt();
+
+            double total = ida + vuelta;
+
+            System.out.println("Desplazamiento diario (Km).....: " + total);
+
+            double dias = distanciasS / total;
+
+            System.out.printf("Días de Clases/camino........: %d%n",
+                    (int) dias < dias ? (int) dias + 1 : (int) dias);
+
+        } catch (Exception e) {
+            System.out.println("Error de entrada");
+
+        } finally {
+            SCN.nextLine();
+        }
     }
-    
+
 }
